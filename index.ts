ec79d49117fa75ffa4107b7e8a0f814031dda727
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-04-01 20:12:45 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,42 @@
+/*npm init (Node PAckage Manager)
+tsc --init (to initialize typescript)
+create a new tsconfig.json
+line 28 .... tsconfig.json (change year 2022 and change module "nodeNext")
+line 30 .... model resulution "nodeNext"
+create module in package.json
+inquirer.js
+mpm js .... is a package.
+npm i inquirer (cmd.... same directory)
+Use npm install <package> afterwards to install a package and save it as dependancy in the package.json file.
+create new file vs code index.ts
+cmd npm i --save-dev @types/inquirer*/
+
+#! usr/bin/env Node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter First Number:", type: "number", name: "firstNumber" },
+  {message: "Enter Second Number:", type: "number", name: "secondNumber"},
+  {message: "Select one of the operator to perform Operation:", 
+  type: "list", 
+  name: "operator", 
+  choices: ["Addition", "Subtraction", "Division", "Multiplication"], 
+},
+]);
+
+
+// Conditional Statement
+if (answer.operator === "Addition") {
+console.log(answer.firstNumber + answer.secondNumber)
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber)
+} 
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber)
+} 
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber)
+} 
+else {
+    console.log("Select a Valid Operator")
+}
